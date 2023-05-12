5ea47d16b19a4334504a265749880e42c710e4dc
westruk <64413688+westruk@users.noreply.github.com>
2023-05-12 11:43:31 +0500

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,6 +4,9 @@ Following properties will be changed:
  * gsm.sim.operator.alpha: O2
  * gsm.sim.operator.numeric: 23402
  * gsm.sim.operator.iso-country: gb
+ * gsm.operator.numeric: 23402
+ * gsm.operator.iso-country: gb
+ * gsm.operator.alpha: O2
 
 These packages will be cleared on next reboot:
  * com.google.android.gms
