45937f2da35e3e171e96fd0f118fb422225c399a
westruk <64413688+westruk@users.noreply.github.com>
2023-10-05 10:22:33 +0500

    Update service.sh

--- service.sh
+++ service.sh
@@ -7,6 +7,7 @@ sleep 30s # wait for boot complete
 if [ -f $MODPATH/pm-clear ]; then
   su -c 'pm clear com.google.android.gms'
   su -c 'pm clear com.google.android.gsf'
+  su -c 'pm clear com.android.vending'
   rm $MODPATH/pm-clear
 fi
 
