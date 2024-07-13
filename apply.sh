db9e4812eb5dbb66d1a4b43d8b03adc6a88095c2
dadaya <vaytedirar@gmail.com>
2024-07-13 11:21:11 +0300

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,9 +1,9 @@
 PROPERTY='
-gsm.sim.operator.numeric=23402
-gsm.sim.operator.iso-country=gb
+gsm.sim.operator.numeric=62120
+gsm.sim.operator.iso-country=NG
 gsm.sim.operator.alpha=O2
-gsm.operator.numeric=23402
-gsm.operator.iso-country=gb
+gsm.operator.numeric=62120
+gsm.operator.iso-country=NG
 gsm.operator.alpha=O2
 '
 
